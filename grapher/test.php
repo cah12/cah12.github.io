5d8b8cc4055562b7bd7da5115a5ea1145e8f7cf2
cah12 <corwin_hope@yahoo.com>
2020-07-06 02:25:52 -0400

    M

--- grapher/test.php
+++ grapher/test.php
@@ -1,8 +1,5 @@
 <?php
 	echo "Hello World";
-	console.log(455);
 	echo $_GET['name'];
-	echo $_GET['address'];
-	console.log($_GET['name']);
-	console.log($_GET['address']);
+	echo $_GET['address'];	
 ?>
